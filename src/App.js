79d0c00ebe7a75f183340ab2c4b1e01789764420
WinDev <khambazarovrenat@gmail.com>
2023-04-24 02:39:40 +0200

    fix bugs

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 export const App = () => {
   const API_KEY = process.env.REACT_APP_ACCESS_KEY;
   const [convertFrom, setConvertFrom] = useState("");
-  const [amount, setAmount] = useState(0);
-  const [result, setResult] = useState(0);
+  const [amount, setAmount] = useState("");
+  const [result, setResult] = useState("");
 
   const handleCurrencyFrom = (e) => {
     const from = e.target.value;
@@ -17,58 +17,80 @@ export const App = () => {
     setAmount(amount);
   };
 
+  const swapDotAndComma = (number) => {
+    const replaceDotWithSpace = number.replace(".", " ");
+    const replaceCommaWithDot = replaceDotWithSpace.replace(/,/g, ".");
+    const replaceSpaceWithComma = replaceCommaWithDot.replace(" ", ",");
+    return replaceSpaceWithComma;
+  };
+
   const convertCurrency = async (e) => {
-    e.preventDefault();
-    const APIURL = `http://api.coinlayer.com/api/live?access_key=${API_KEY}`;
-    const fetchURL = `${APIURL}&from=${convertFrom.toUpperCase()}&amount=${amount}`;
-    const responseData = await fetch(fetchURL);
-    const jsonData = await responseData.json();
-    const convertResult = jsonData.rates[convertFrom] * amount;
-
-    const replaceDotToComma = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-    }).format(convertResult);
-
-    setResult(replaceDotToComma);
+    if (!convertFrom || !amount || amount === "0") {
+      return;
+    }
+    try {
+      e.preventDefault();
+      const APIURL = `http://api.coinlayer.com/api/live?access_key=${API_KEY}`;
+      const fetchURL = `${APIURL}&from=${convertFrom.toUpperCase()}&amount=${amount}`;
+      const responseData = await fetch(fetchURL);
+      if (!responseData.ok) {
+        throw new Error("API response is unsuccessful");
+      }
+      const jsonData = await responseData.json();
+      if (jsonData.error) {
+        throw new Error(jsonData.error.info);
+      }
+      const convertResult = jsonData.rates[convertFrom] * amount;
+      const replaceDotToComma = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(convertResult);
+      setResult(replaceDotToComma);
+    } catch (error) {
+      console.error(error);
+      setResult(`Error: ${error.message}`);
+    }
   };
 
   return (
     <div className='App'>
       <h1 className='header'>Crypto Converter</h1>
-      <h2 className='result'>{result && `${result}`}</h2>
+      <div className='container'>
+        <h2 className='result'>
+          {/* BTC */}
+          {result !== "" && result !== "0" && amount !== "1"
+            ? `${amount} ${convertFrom}'s`
+            : result !== "" && result !== "0" && amount === "1"
+            ? `${amount} ${convertFrom}`
+            : null}
+        </h2>
+        <h2 className='result usd'>
+          {/* 111 */}
+          {result !== "0" && swapDotAndComma(result)}
+        </h2>
+      </div>
       <form className='form' action='#'>
-        <div className='wrapper'>
-          <select
-            className='select-crypto'
-            name='select-crypto'
-            onChange={handleCurrencyFrom}
-          >
-            <option className='option-crypto'>Select</option>
-            <option className='option-crypto' value='btc'>
-              BTC
-            </option>
-            <option className='option-crypto' value='eth'>
-              ETH
-            </option>
-            <option className='option-crypto' value='ada'>
-              ADA
-            </option>
-            <option className='option-crypto' value='xrp'>
-              XRP
-            </option>
-            <option className='option-crypto' value='ltc'>
-              LTC
-            </option>
-          </select>
-          <input
-            required
-            className='form-input'
-            type='number'
-            placeholder='Amount'
-            onChange={handleCurrencyAmount}
-          />
-        </div>
+        <select
+          required
+          className='select-crypto'
+          name='select-crypto'
+          onChange={handleCurrencyFrom}
+        >
+          <option value=''>Choose a crypto</option>
+          <option value='btc'>BTC - Bitcoin </option>
+          <option value='eth'>ETH - Ethereum </option>
+          <option value='ada'>ADA - Cardano </option>
+          <option value='xrp'>XRP - Ripple </option>
+          <option value='ltc'>LTC - Litecoin </option>
+        </select>
+        <input
+          min={0}
+          required
+          className='form-input'
+          type='number'
+          placeholder='Amount'
+          onChange={handleCurrencyAmount}
+        />
         <button
           className='form-submit-btn'
           type='submit'
